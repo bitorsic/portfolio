457b2a6eee3100ed4ccca80fbf80c4d83081fb58
Yash Jaiswal <bitorsic@protonmail.com>
2024-07-13 22:35:24 +0530

    Initial Commit

---
+++ src/data/constants.js
@@ -0,0 +1,216 @@
+export const Bio = {
+  name: "Yash Jaiswal",
+  roles: [
+    "Full Stack Developer",
+    "Cloud Engineer",
+    "Cybersecurity Engineer",
+  ],
+  github: "https://github.com/bitorsic",
+  linkedin: "https://www.linkedin.com/in/bitorsic/",
+};
+
+export const skills = [
+  {
+    title: "Full Stack Development",
+    skills: [
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+      {
+        name: "Express.js",
+        image: "https://w7.pngwing.com/pngs/925/447/png-transparent-express-js-node-js-javascript-mongodb-node-js-text-trademark-logo.png"
+      },
+      {
+        name: "React.js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "Node.js",
+        image: "https://nodejs.org/static/images/logo.svg",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      {
+        name: "Postgresql",
+        image: "https://www.postgresql.org/media/img/about/press/elephant.png",
+      },
+    ],
+  },
+  {
+    title: "Languages",
+    skills: [
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "C++",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/ISO_C%2B%2B_Logo.svg/911px-ISO_C%2B%2B_Logo.svg.png",
+      },
+      {
+        name: "Java",
+        image:
+          "https://static-00.iconduck.com/assets.00/java-original-icon-756x1024-j3tx11wk.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+    ],
+  },
+  {
+    title: "Cloud Computing",
+    skills: [
+      {
+        name: "Google Cloud",
+        image:
+          "https://static-00.iconduck.com/assets.00/google-cloud-icon-1024x823-wiwlyizc.png",
+      },
+      {
+        name: "AWS",
+        image:
+          "https://cdn.iconscout.com/icon/free/png-256/free-aws-1869025-1583149.png?f=webp",
+      },
+      {
+        name: "Azure",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Microsoft_Azure.svg/2048px-Microsoft_Azure.svg.png",
+      },
+      {
+        name: "Firebase",
+        image: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://d2366e79lh4q4u.cloudfront.net/wp-content/uploads/2024/05/Logo.png",
+    role: "DevOps Engineer Intern",
+    company: "PearlThoughts",
+    date: "Oct 2023 - Nov 2023",
+  },
+  {
+    id: 0,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjvkSPG71j5yHEJl3MKxYNmo_HJHizwn2fnA&s",
+    role: "Cloud Lead",
+    company: "GDSC PESMCOE",
+    date: "Aug 2023 - Present",
+  },
+  {
+    id: 0,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjvkSPG71j5yHEJl3MKxYNmo_HJHizwn2fnA&s",
+    role: "Web Development Team Member",
+    company: "GDSC PESMCOE",
+    date: "Aug 2023 - Present",
+  },
+  {
+    id: 0,
+    img: "https://sdn.signalhire.co/storage/company/7d69/5ece/ac21/b513/6466/665e/714b/1f6b.webp",
+    role: "Technical Team Member",
+    company: "Blockaders Club",
+    date: "Oct 2023 - Present",
+  },
+  {
+    id: 0,
+    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8e/Association_for_Computing_Machinery_%28ACM%29_logo.svg/2048px-Association_for_Computing_Machinery_%28ACM%29_logo.svg.png",
+    role: "Technical Team Member",
+    company: "PESMCOE ACM Student Chapter",
+    date: "Mar 2023 - Apr 2023",
+  },
+  {
+    id: 0,
+    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8e/Association_for_Computing_Machinery_%28ACM%29_logo.svg/2048px-Association_for_Computing_Machinery_%28ACM%29_logo.svg.png",
+    role: "Documentation Team Member",
+    company: "PESMCOE ACM Student Chapter",
+    date: "Mar 2023",
+  },
+  {
+    id: 0,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQA8YqfEgHQTmnlfKZdZUM7xZuZVv5AUXeguA&s",
+    role: "Customer Support Intern",
+    company: "Valethi Technologies",
+    date: "Apr 2021 - Jun 2021",
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTizYrqqdfhotUM7bZU8OYx0520hoqwVI-miA&s",
+    school: "Progressive Education Society's Modern College of Engineering",
+    date: "2022 - 2025",
+    degree: "BE, Computer Engineering",
+  },
+  {
+    id: 1,
+    img: "https://ik.imagekit.io/lyzj6ywpw/logo/mit-world-peace-university.png",
+    school: "MIT Polytechnic",
+    date: "2020 - 2022",
+    degree: "Diploma in Computer Engineering",
+  },
+];
+
+export const projects = [
+  {
+    id: 9,
+    title: "Chat-Nat",
+    description:
+      `Messaging webapp which implements End-to-End Encryption for all messages; 
+      Used ECDH (Elliptic Curve Diffie Hellman) key exchange algorithm, and AES encryption;
+      Used the MERN (MongoDB Express React Node.js) stack`,
+  },
+  {
+    id: 0,
+    title: "Postomania",
+    description:
+      `Backend for a social media platform, where users can create and interact with posts;
+      Used Node.js, JWT Authentication;
+      Used symmetric encryption to encrypt the content of posts and comments`,
+  },
+  {
+    id: 1,
+    title: "Washer Production Backend",
+    description:
+      `Backend for an Inventory Management system for a company that produces washers;
+      Used Node.js and its packages: express.js, sequelize, bcrypt;
+      Used AWS Relational Database Service (RDS) to host database on the cloud temporarily`,
+  },
+  {
+    id: 2,
+    title: "Epic NFT",
+    date: "Jan 2023 - Mar 2023",
+    description:
+      `Dapp which mints a new random NFT to a wallet;
+      Used Node.js, Solidity;
+      Used MetaMask extension to connect wallet`,
+  },
+  {
+    id: 3,
+    title: "Trivia Webapp",
+    date: "Dec 2021 - Apr 2022",
+    description:
+      `Created the backend for a Trivia webapp used for ACM Invicta 2K23;
+      Used Node.js libraries- Express and Sequelize;
+      The database was hosted on AWS RDS`,
+  },
+  {
+    id: 10,
+    title: "Weather Mobile App",
+    description:
+      `Flutter app made at GDSC Flutter Forward Extended 2023;
+      Fetches user's location and provides weather details;
+      Used Firebase and API provided by OpenWeatherMap`,
+  }
+];
